5bbc5cc3e2b9ffef82e6f0b21dcddf29550d3d82
Rohit <rohitkumarpattanayak@gmail.com>
2022-02-06 12:52:46 +0530

    finished with few flaws

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// var x = "rohit kumar pattanayak";
+
+// function nme() {
+//   console.log("hi");
+//   document.getElementById("rohit").innerHTML = x;
+// }
+// function rn() {
+//   nme();
+// }
